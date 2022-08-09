f95302d6285b01609aac855b24fbf330d39455d0
madhuri605 <80457300+madhuri605@users.noreply.github.com>
2022-08-09 18:25:34 +0530

    crud operation

---
+++ src/student/student.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { StudentService } from './student.service';
+
+@Controller('student')
+export class StudentController {
+  constructor(private studentService: StudentService) {}
+
+  @Get()
+  getAllStudents() {
+    return this.studentService.getStudents();
+  }
+
+  @Get(':index')
+  getSpecificStudent(@Param() index) {
+    return this.studentService.getSpecificStudent(index.index);
+  }
+
+  @Post()
+  createStudent(@Body() student) {
+    return this.studentService.createStudent(student);
+
+  }
+
+  @Delete(':id')
+  deleteStudent(@Param() index) {
+    return this.studentService.deleteStudent(index.id);
+  }
+
+  @Patch(':id')
+  updateStudent(@Param() id, @Body() student) {
+    return this.studentService.updateStudent(id.id, student);
+  }
+  @Put()
+  updateAllStudent(@Body() student){
+    return this.studentService.updatedAllStudent(student);
+  }
+
+}
